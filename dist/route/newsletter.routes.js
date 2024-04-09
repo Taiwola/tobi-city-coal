2a9c52806790b31adab4f6d2709ab6ffbe464d1f
Intuneteq <tobiolanitori@gmail.com>
2024-04-09 07:19:04 +0100

    chore: untracking dist

--- dist/route/newsletter.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.newletterRouter = void 0;
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-exports.newletterRouter = router;
-const controller_1 = require("../controller");
-router.post("/sendFirstNewsletter", controller_1.sendFirstMail);
-//# sourceMappingURL=newsletter.routes.js.map
