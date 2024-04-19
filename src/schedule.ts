01b4095cecc313e4b3347477d8361789e0282b2e
Taiwola <seunolanitori@gmail.com>
2024-04-19 14:21:16 +0100

    shedule event

--- src/schedule.ts
+++ src/schedule.ts
@@ -7,13 +7,19 @@ import {
   three_days_reminder,
   two_weeks_reminder,
 } from "./lib/mailer";
+import { slackApp } from "./config/slack.config";
 
 const runScheduler = () => {
   console.log("Waiting to run scheduled jobs");
 
-  const date = new Date(2024, 3, 10, 2, 25, 0);
+  // const date = new Date(2024, 3, 10, 2, 25, 0);
 
-  schedule.scheduleJob("Two weeks to marathon", date, async () => {
+  
+  const marathonDate = new Date("2024-05-04T10:00:00");
+  const twoWeeksDate = new Date(marathonDate);
+  twoWeeksDate.setDate(twoWeeksDate.getDate() - 14);
+
+  schedule.scheduleJob("Two weeks to marathon", twoWeeksDate, async () => {
     const payments = await findAll();
 
     const promises = await Promise.all(
@@ -32,6 +38,12 @@ const runScheduler = () => {
             console.log(errorMessage);
 
             // LOG TO SLACK
+            await slackApp.client.chat.postMessage({
+              token: process.env.SLACK_BOT_TOKEN as string,
+              channel: process.env.SLACK_CHANNEL as string,
+              text: errorMessage,
+            });
+            throw new Error(errorMessage);
             // return errorMessage;
           }
 
@@ -40,7 +52,10 @@ const runScheduler = () => {
     );
   });
 
-  schedule.scheduleJob("One week to marathon", date, async () => {
+  const oneWeekDate = new Date(marathonDate);
+  oneWeekDate.setDate(oneWeekDate.getDate() - 7);
+
+  schedule.scheduleJob("One week to marathon", oneWeekDate, async () => {
     const payments = await findAll();
 
     const promises = await Promise.all(
@@ -59,6 +74,12 @@ const runScheduler = () => {
             console.log(errorMessage);
 
             // LOG TO SLACK
+            await slackApp.client.chat.postMessage({
+              token: process.env.SLACK_BOT_TOKEN as string,
+              channel: process.env.SLACK_CHANNEL as string,
+              text: errorMessage,
+            });
+            throw new Error(errorMessage);
             // return errorMessage;
           }
 
@@ -67,7 +88,9 @@ const runScheduler = () => {
     );
   });
 
-  schedule.scheduleJob("Three days to marathon", date, async () => {
+  const threeDaysDate = new Date(marathonDate);
+  threeDaysDate.setDate(threeDaysDate.getDate() - 3);
+  schedule.scheduleJob("Three days to marathon", threeDaysDate, async () => {
     const payments = await findAll();
 
     const promises = await Promise.all(
@@ -86,6 +109,12 @@ const runScheduler = () => {
             console.log(errorMessage);
 
             // LOG TO SLACK
+            await slackApp.client.chat.postMessage({
+              token: process.env.SLACK_BOT_TOKEN as string,
+              channel: process.env.SLACK_CHANNEL as string,
+              text: errorMessage,
+            });
+            throw new Error(errorMessage);
             // return errorMessage;
           }
 
@@ -94,7 +123,9 @@ const runScheduler = () => {
     );
   });
 
-  schedule.scheduleJob("Day of marathon", date, async () => {
+  const dayOfEventDate = new Date(marathonDate);
+  dayOfEventDate.setHours(7, 0, 0, 0);
+  schedule.scheduleJob("Day of marathon", dayOfEventDate, async () => {
     const payments = await findAll();
 
     const promises = await Promise.all(
@@ -113,6 +144,12 @@ const runScheduler = () => {
             console.log(errorMessage);
 
             // LOG TO SLACK
+            await slackApp.client.chat.postMessage({
+              token: process.env.SLACK_BOT_TOKEN as string,
+              channel: process.env.SLACK_CHANNEL as string,
+              text: errorMessage,
+            });
+            throw new Error(errorMessage);
             // return errorMessage;
           }
 
